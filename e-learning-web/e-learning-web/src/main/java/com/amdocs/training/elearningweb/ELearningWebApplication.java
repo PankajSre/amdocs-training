c161c76e315265b62f353126e97dc40402084e6c
PankajSre <pankaj.saini85@gmail.com>
2021-09-18 09:03:08 +0530

    Springboot Meb MVC

---
+++ e-learning-web/e-learning-web/src/main/java/com/amdocs/training/elearningweb/ELearningWebApplication.java
@@ -0,0 +1,13 @@
+package com.amdocs.training.elearningweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELearningWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELearningWebApplication.class, args);
+	}
+
+}
---
+++ e-learning-web/e-learning-web/src/main/java/com/amdocs/training/elearningweb/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.amdocs.training.elearningweb;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ELearningWebApplication.class);
+	}
+
+}
